fc715ce133f7eb61997dc0dc8de92289cc08b6a9
Alejandro Romero Gill <itsaxxerbusiness@gmail.com>
2026-08-03 14:50:23 +0000

    Fixing admin panel css

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
+import "./admin.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
